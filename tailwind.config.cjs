05ab9dfe0bd861b574ff9227cd32377d4386ba66
Mattxm <mattxm@live.com>
2022-09-26 13:35:15 -0700

    styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {light: "#f5f5f5", dark: "#2b2b2b"},
+        primary: {light: "#e0e0e0", dark: "#2b2b2b"},
         secondary: {light: "#f7f7f7", dark: "#1c1c1c"},
         thirdary: "#363636",
         highlight: "#f2ef8a",
