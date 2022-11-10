ee19861bb4bfde2238a6b5abad810e29dc823776
Mattxm <mattxm@live.com>
2022-11-10 01:10:21 -0800

    added delete feature for posts

--- src/routes/(app)/post/[post_id]/+page.server.js
+++ src/routes/(app)/post/[post_id]/+page.server.js
@@ -1,14 +1,33 @@
 import { supabase } from '$lib/supabaseClient'
+import { redirect } from '@sveltejs/kit'
+import { data } from 'autoprefixer'
 
 export const load = async ({ params }) => {
-    let { data: posts, error } = await supabase
+    let { data: post, error } = await supabase
         .from('posts')
         .select(
             '*, profiles(username, avatar_url), comments(*, profiles(username, avatar_url))'
         )
         .eq('post_id', params.post_id)
         .single()
+
+    if (error) throw redirect(301, '/')
+
+    if (post.deleted)
+        return {
+            post: {
+                deleted: post.deleted,
+                post_id: post.post_id,
+                created_at: post.created_at,
+                content: 'deleted',
+                tags: post.tags,
+                title: post.title,
+                comments: post.comments,
+                profiles: { username: 'deleted', avatar_url: null },
+            },
+        }
+
     return {
-        post: posts,
+        post: post,
     }
 }
--- src/routes/(app)/post/[post_id]/+page.svelte
+++ src/routes/(app)/post/[post_id]/+page.svelte
@@ -6,29 +6,109 @@
         SquareIcon,
         ThumbsDownIcon,
         ThumbsUpIcon,
+        XIcon,
     } from 'svelte-feather-icons'
     import Comment from '$lib/comment.svelte'
     import { marked } from 'marked'
     import { page } from '$app/stores'
+    import { currentUser } from '$lib/userStore'
+    import { supabase } from '$lib/supabaseClient'
+    import { goto } from '$app/navigation'
+    import {
+        Dialog,
+        DialogDescription,
+        DialogOverlay,
+        DialogTitle,
+    } from '@rgossiaux/svelte-headlessui'
 
     let source = ``
     //marked.setOptions({gfm:true})
     $: markdown = marked.parse(source)
     $: post = $page.data.post
-    $: dateposted = new Date(post.created_at).toLocaleString()
-    $: tags = post.tags.split(',')
+    $: dateposted = new Date(post.created_at).toLocaleString() ?? null
+    $: tags = post.tags ? post.tags.split(',') : []
     let ShowPreview = false
+    let DeleteOpen = false
+
+    async function DeletePost() {
+        const id = (await supabase.auth.getUser()).data.user?.id
+
+        // const { data, error } = await supabase
+        //     .from('posts')
+        //     .delete()
+        //     .eq('post_id', post?.post_id)
+        //     .eq('user_id', id)
+
+        const { data, error } = await supabase
+            .from('posts')
+            .update({ deleted: true })
+            .eq('post_id', post?.post_id)
+            .eq('user_id', id)
+
+        if (!error) {
+            await goto(`/post/${post.post_id}`)
+        }
+    }
 </script>
+
+<Dialog
+    open={DeleteOpen}
+    on:close={() => (DeleteOpen = false)}
+    class="fixed inset-0"
+>
+    <DialogOverlay class="fixed inset-0 z-0 bg-black bg-opacity-25" />
+    <div
+        class="pointer-events-none fixed inset-0 flex items-center justify-center overflow-y-auto"
+    >
+        <div
+            class=" pointer-events-auto relative overflow-hidden rounded-md bg-secondary-light p-4 dark:bg-secondary-dark"
+        >
+            <button
+                on:click={() => (DeleteOpen = false)}
+                class="absolute top-0 right-0 mr-4 mt-4"
+            >
+                <XIcon size="24" />
+            </button>
+            <DialogTitle class="text-lg">Delete Post</DialogTitle>
+            <div
+                class="mb-2 flex h-px bg-secondary-light dark:bg-secondary-dark "
+            />
+            <DialogDescription class="">
+                Are you sure you want to permanently delete this post?
+            </DialogDescription>
+            <div class="mt-2 text-right">
+                <button
+                    class="c-text px-2 py-0.5"
+                    on:click={() => (DeleteOpen = false)}
+                >
+                    Cancel
+                </button>
+                <button
+                    class=" rounded-sm bg-red-500 px-2 py-0.5 hover:bg-red-600 "
+                    on:click={() => {
+                        DeleteOpen = false
+                        DeletePost()
+                    }}
+                >
+                    Delete
+                </button>
+            </div>
+        </div>
+    </div>
+</Dialog>
 
 <div class="mx-auto flex min-h-screen max-w-4xl px-2 md:px-4">
     <div class="mt-4 flex flex-1 flex-col">
         <span class="flex text-sm text-neutral-400">
             <span class="">
-                Created by
-                <a href={`/user/${post.profiles.username}`}>
-                    {post.profiles.username}
-                </a>
-                - {timeBetween(post.created_at)}
+                {#if post.deleted}
+                    Deleted by user
+                {:else}Created by
+                    <a href={`/user/${post.profiles.username}`}>
+                        {post.profiles.username}
+                    </a>
+                {/if}
+                - posted {timeBetween(post.created_at)}
             </span>
         </span>
         <span class="pb-2 text-lg">{post.title}</span>
@@ -58,6 +138,7 @@
             >
                 <LinkIcon size="16" />
             </button>
+
             <button
                 on:click={() => {
                     document.getElementById('comments')?.scrollIntoView(true)
@@ -67,6 +148,19 @@
                 <SquareIcon size="16" />
                 <span class="ml-2 text-sm">{post.comments.length} comments</span
                 >
+            </button>
+
+            <button
+                on:click={() => {
+                    DeleteOpen = true
+                }}
+                class={`${
+                    $currentUser.user_id == post?.user_id
+                        ? 'visible'
+                        : 'invisible'
+                } secondary-text flex items-center text-sm`}
+            >
+                delete
             </button>
         </span>
 
